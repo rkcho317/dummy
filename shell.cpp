56a37c200040916f3c1dd974904f81dd01a7e57d
Rosa <rkcho317@csu.fullerton.edu>
2020-09-24 19:28:13 -0700

    CPSC 351: Shell.cpp

---
+++ shell.cpp
@@ -0,0 +1,65 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	/* The command buffer */
+	string cmdBuff;
+
+	/* The ID of the child process */
+	pid_t pid;
+
+	/* Keep running until the user has typed "exit" */
+	do
+	{
+		/* Prompt the user to enter the command */
+		cerr << "cmd>";
+		cin >> cmdBuff;
+
+		/* If the user wants to exit */
+		if (cmdBuff != "exit")
+		{
+			/* TODO: Create a child */
+
+			pid == fork();
+
+			/* TODO: Error check to make sure the child was successfully created */
+			if (pid < 0) {
+				/*** TODO: If I am child, I will do this: ****/
+			/* Call execlp() to replace my program with that specified at the command line.
+			 * PLEASE NOTE: YOU CANNOT PASS cmdBuff DIRECTLY to execlp(). It is because
+			 * cmdBuff is an object of type string (i.e., a class) and execlp() expects
+			 * an array of characters.  However, you can pass cmdBuff.c_str(), which will
+			 * return an array of characters representation of the string object.
+			 *
+			 * Also, please do not forget to error check your exelp() system calls.
+			 */
+				perror("fork");
+				exit(-1);
+				return;
+			}
+				/*** TODO: If I am a parent, I will do the following ***?
+				/* Wait for the child process to terminate */
+			else if (pid == 0) {
+				execlp("/bin/ls", "ls",NULL);
+			}
+
+			else {
+				wait(NULL);
+				return;
+			}
+
+
+
+		}
+	} while (cmdBuff != "exit");
+
+	return 0;
+}
